1be1469d9b5390f56a259df69bd05e0a3ce01d30
Mahnoud14M <mahmoud1420m@gmail.com>
2019-06-29 02:53:42 +0200

    gallary

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
@@ -11,6 +11,31 @@ export class GalleryComponent implements OnInit {
 
   ngOnInit() {
     window.scrollTo(0, 0);
+    window.scrollTo(0, 0);
+    $('#card').mouseenter(() => {
+      $('#innerCard').slideDown(1000);
+    });
+    $('#card').mouseleave(() => {
+      $('#innerCard').slideUp(1000);
+    });
+    $('#card2').mouseenter(() => {
+      $('#innerCard2').slideDown(1000);
+    });
+    $('#card2').mouseleave(() => {
+      $('#innerCard2').slideUp(1000);
+    });
+    $('#card3').mouseenter(() => {
+      $('#innerCard3').slideDown(1000);
+    });
+    $('#card3').mouseleave(() => {
+      $('#innerCard3').slideUp(1000);
+    });
+    $('#card4').mouseenter(() => {
+      $('#innerCard4').slideDown(1000);
+    });
+    $('#card4').mouseleave(() => {
+      $('#innerCard4').slideUp(1000);
+    });
   }
 
 }
